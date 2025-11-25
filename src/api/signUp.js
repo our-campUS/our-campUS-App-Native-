a2c22139f33273eecf875f1f09656d1cacb5fc1b
jaesangkim000 <kjs564821@gmail.com>
2025-11-25 19:29:58 +0900

    refactor : env 파일에 카카오 네이티브 키 저장

--- src/api/signUp.js
+++ src/api/signUp.js
@@ -1,7 +1,8 @@
 import { initializeKakaoSDK } from '@react-native-kakao/core';
 import { login } from '@react-native-kakao/user';
+import { KAKAO_NATIVE_APP_KEY } from '@env';
 
-const KAKAO_NATIVE_APP_KEY = 'cf9915ad21d898963c9b449516ca45fb';
+// const KAKAO_NATIVE_APP_KEY = 'cf9915ad21d898963c9b449516ca45fb';
 
 export function initKakao() {
   initializeKakaoSDK(KAKAO_NATIVE_APP_KEY);
