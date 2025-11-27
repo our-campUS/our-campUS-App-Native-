9eb93f9bd8fef5c61d45b7b6d53f40459e24388d
daeun088 <kde2329@naver.com>
2025-11-27 17:33:05 +0900

    chore: fontWeight 수정

--- src/style/typography.js
+++ src/style/typography.js
@@ -7,21 +7,21 @@ export const typography = {
   // === Display ===
   display1: {
     fontSize: 44,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(44, 132),
     letterSpacing: letterSpacing(44, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   display2: {
     fontSize: 32,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(32, 132),
     letterSpacing: letterSpacing(32, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   display3: {
     fontSize: 28,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(28, 132),
     letterSpacing: letterSpacing(28, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -30,42 +30,42 @@ export const typography = {
   // === Heading ===
   heading1: {
     fontSize: 28,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(28, 132),
     letterSpacing: letterSpacing(28, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   heading2: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(24, 128),
     letterSpacing: letterSpacing(24, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   heading3: {
     fontSize: 22,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(22, 128),
     letterSpacing: letterSpacing(22, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   heading4: {
     fontSize: 19,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(19, 128),
     letterSpacing: letterSpacing(19, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   heading5: {
     fontSize: 17,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(17, 128),
     letterSpacing: letterSpacing(17, -2),
     fontFamily: FONT_FAMILY_BOLD,
   },
   heading6: {
     fontSize: 15,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(15, 128),
     letterSpacing: letterSpacing(15, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -74,7 +74,7 @@ export const typography = {
   // === Body ===
   body1Bold: {
     fontSize: 19,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(19, 155),
     letterSpacing: letterSpacing(19, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -88,7 +88,7 @@ export const typography = {
   },
   body2Bold: {
     fontSize: 17,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(17, 155),
     letterSpacing: letterSpacing(17, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -102,7 +102,7 @@ export const typography = {
   },
   body3Bold: {
     fontSize: 15,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(15, 155),
     letterSpacing: letterSpacing(15, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -116,7 +116,7 @@ export const typography = {
   },
   body4Bold: {
     fontSize: 13,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(13, 155),
     letterSpacing: letterSpacing(13, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -132,7 +132,7 @@ export const typography = {
   // === Caption ===
   caption1Bold: {
     fontSize: 12,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(12, 126),
     letterSpacing: letterSpacing(12, -2),
     fontFamily: FONT_FAMILY_BOLD,
@@ -146,7 +146,7 @@ export const typography = {
   },
   caption2Bold: {
     fontSize: 10,
-    fontWeight: '600',
+    fontWeight: '700',
     lineHeight: lineHeight(10, 126),
     letterSpacing: letterSpacing(10, -2),
     fontFamily: FONT_FAMILY_BOLD,
