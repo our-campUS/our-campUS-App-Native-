bdc175041bc276d59cc71b65b7e094bdf9008000
daeun088 <kde2329@naver.com>
2025-12-05 01:28:05 +0900

    feat: 지도 화면 진입 시 위치 권한 요청

--- src/screens/Map/MapScreen.js
+++ src/screens/Map/MapScreen.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { NaverMapView } from '@mj-studio/react-native-naver-map';
 
+import {
+  request,
+  PERMISSIONS,
+  RESULTS,
+  requestMultiple,
+} from 'react-native-permissions';
+
 const MapScreen = () => {
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        if (Platform.OS === 'ios') {
+          const result = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+          if (result === RESULTS.BLOCKED) {
+            console.log('iOS: 권한이 거부되었습니다. 설정에서 허용해주세요.');
+          }
+        } else if (Platform.OS === 'android') {
+          const result = await requestMultiple([
+            PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+            PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+          ]);
+
+          console.log('Android Permissions:', result);
+        }
+      } catch (e) {
+        console.error('권한 요청 실패:', e);
+      }
+    };
+
+    requestLocationPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
       <NaverMapView
