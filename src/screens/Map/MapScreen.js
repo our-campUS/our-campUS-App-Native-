5b7ce819b8488a0777fd65ceef1d9ab1e0422a49
daeun088 <kde2329@naver.com>
2025-12-04 23:55:01 +0900

    feat: android naver map 설정

--- src/screens/Map/MapScreen.js
+++ src/screens/Map/MapScreen.js
@@ -1,11 +1,43 @@
-import { Text, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NaverMapView } from '@mj-studio/react-native-naver-map';
 
 const MapScreen = () => {
   return (
-    <ScrollView>
-      <Text>Map Screen</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <NaverMapView
+        style={{ flex: 1 }}
+        initialCamera={{
+          latitude: 37.5665,
+          longitude: 126.978,
+          zoom: 16,
+        }}
+        isShowLocationButton={true}
+      />
+
+      <View style={styles.overlay}>
+        <Text style={styles.text}>네이버 지도 테스트</Text>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+});
 
 export default MapScreen;
