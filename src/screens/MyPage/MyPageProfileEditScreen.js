063acc8b13f6123f007f19fbbad301cea8f0f20c
jaesangkim000 <kjs564821@gmail.com>
2026-01-05 21:22:15 +0900

    feat : 로그아웃 ui

--- src/screens/MyPage/MyPageProfileEditScreen.js
+++ src/screens/MyPage/MyPageProfileEditScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable, Modal } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../../style/colors';
 import LabelTitle from '../../components/LabelTitle';
@@ -6,57 +6,104 @@ import typography from '../../style/typography';
 import defaultProfileImage from '../../../assets/defaultProfileImage.png';
 import EditIcon from '../../../assets/EditImage.svg';
 import ArrowRightIcon from '../../../assets/ArrowRightIcon.svg';
+import { useState } from 'react';
+import Button from '../../components/Button';
 
 const MyPageProfileEditScreen = ({ navigation }) => {
+  const [isLogoutModalVisible, setIsLogoutModalVisible] = useState(false);
   return (
-    <SafeAreaView style={styles.container}>
-      <LabelTitle
-        title="프로필 설정"
-        navigation={navigation}
-        useBackButton={true}
-        onPressBack={() => navigation.goBack()}
-      />
-      <View style={styles.profileImageWrapper}>
-        <Image source={defaultProfileImage} style={styles.profileImage} />
-        <Pressable style={styles.editIcon}>
-          <EditIcon width={24} height={24} />
-        </Pressable>
-      </View>
-      <Text style={styles.nickname}>닉넴 뭐하지</Text>
-      <View style={styles.profileInfoWrapper}>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>닉네임</Text>
-          <Pressable onPress={() => navigation.navigate('EditNicknameScreen')}>
-            <ArrowRightIcon width={10} height={10} />
+    <>
+      <SafeAreaView style={styles.container}>
+        <LabelTitle
+          title="프로필 설정"
+          navigation={navigation}
+          useBackButton={true}
+          onPressBack={() => navigation.goBack()}
+        />
+        <View style={styles.profileImageWrapper}>
+          <Image source={defaultProfileImage} style={styles.profileImage} />
+          <Pressable style={styles.editIcon}>
+            <EditIcon width={24} height={24} />
           </Pressable>
         </View>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>학적정보</Text>
-          <ArrowRightIcon width={10} height={10} />
+        <Text style={styles.nickname}>닉넴 뭐하지</Text>
+        <View style={styles.profileInfoWrapper}>
+          <View style={styles.profileInfoItem}>
+            <Text style={styles.profileInfoItemTitle}>닉네임</Text>
+            <Pressable
+              onPress={() => navigation.navigate('EditNicknameScreen')}
+            >
+              <ArrowRightIcon width={10} height={10} />
+            </Pressable>
+          </View>
+          <View style={styles.profileInfoItem}>
+            <Text style={styles.profileInfoItemTitle}>학적정보</Text>
+            <ArrowRightIcon width={10} height={10} />
+          </View>
+          <View style={styles.profileInfoItem}>
+            <Text style={styles.profileInfoItemTitle}>학적정보</Text>
+            <View style={styles.profileInfoItemRightWrapper}>
+              <Text style={styles.profileInfoItemRightText}>미인증</Text>
+              <ArrowRightIcon width={10} height={10} />
+            </View>
+          </View>
+          <View style={styles.profileInfoItem}>
+            <Text style={styles.profileInfoItemTitle}>연결된 계정</Text>
+            <Text style={styles.profileInfoItemRightText}>카카오</Text>
+          </View>
         </View>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>학적정보</Text>
-          <View style={styles.profileInfoItemRightWrapper}>
-            <Text style={styles.profileInfoItemRightText}>미인증</Text>
+        <View style={styles.logoutButtonWrapper}>
+          <Pressable
+            style={styles.profileInfoItem}
+            onPress={() => setIsLogoutModalVisible(true)}
+          >
+            <Text style={styles.profileInfoItemTitle}>로그아웃</Text>
+            <ArrowRightIcon width={10} height={10} />
+          </Pressable>
+          <View style={styles.profileInfoItem}>
+            <Text style={styles.profileInfoItemTitle}>회원 탈퇴</Text>
             <ArrowRightIcon width={10} height={10} />
           </View>
         </View>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>연결된 계정</Text>
-          <Text style={styles.profileInfoItemRightText}>카카오</Text>
+      </SafeAreaView>
+      <Modal
+        visible={isLogoutModalVisible}
+        onRequestClose={() => setIsLogoutModalVisible(false)}
+        transparent={true}
+      >
+        <View style={styles.logoutModalOverlay}>
+          <View style={styles.logoutModalContent}>
+            <Text style={styles.logoutModalTitle}>로그아웃</Text>
+            <Text style={styles.logoutConfirmMessage}>
+              정말로 로그아웃하시겠어요?
+            </Text>
+            <Button
+              title="로그아웃"
+              onPress={() => setIsLogoutModalVisible(false)}
+              style={{
+                width: '100%',
+                height: 50,
+                paddingHorizontal: 10,
+                paddingVertical: 15,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: colors.blue[400],
+                borderRadius: 10,
+                marginTop: 16,
+              }}
+            />
+            <View style={styles.logoutModalCancelTextWrapper}>
+              <Text
+                style={styles.logoutModalCancelText}
+                onPress={() => setIsLogoutModalVisible(false)}
+              >
+                뒤로 가기
+              </Text>
+            </View>
+          </View>
         </View>
-      </View>
-      <View style={styles.logoutButtonWrapper}>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>로그아웃</Text>
-          <ArrowRightIcon width={10} height={10} />
-        </View>
-        <View style={styles.profileInfoItem}>
-          <Text style={styles.profileInfoItemTitle}>회원 탈퇴</Text>
-          <ArrowRightIcon width={10} height={10} />
-        </View>
-      </View>
-    </SafeAreaView>
+      </Modal>
+    </>
   );
 };
 
@@ -119,6 +166,44 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: colors.gray[200],
     gap: 14,
+  },
+  logoutModalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  logoutModalContent: {
+    width: '100%',
+    marginHorizontal: 20,
+    paddingVertical: 24,
+    paddingHorizontal: 20,
+    backgroundColor: colors.common.white,
+    borderRadius: 18,
+    alignItems: 'center',
+  },
+
+  logoutModalTitle: {
+    ...typography.heading4,
+    color: colors.gray[850],
+  },
+  logoutConfirmMessage: {
+    ...typography.body3Regular,
+    color: colors.gray[500],
+    textAlign: 'center',
+    marginTop: 8,
+  },
+  logoutModalCancelTextWrapper: {
+    marginTop: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+  },
+  logoutModalCancelText: {
+    ...typography.heading6,
+    color: colors.gray[700],
   },
 });
 
