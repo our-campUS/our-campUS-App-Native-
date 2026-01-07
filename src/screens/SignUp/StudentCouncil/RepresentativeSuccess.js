c91a25cb8ce6db6c7791983117e9db97bf598040
jaesangkim000 <kjs564821@gmail.com>
2026-01-07 17:47:39 +0900

    feat : 학생회 회원가입 statusBar 스타일 수정

--- src/screens/SignUp/StudentCouncil/RepresentativeSuccess.js
+++ src/screens/SignUp/StudentCouncil/RepresentativeSuccess.js
@@ -61,9 +61,10 @@ const RepresentativeSuccess = ({ navigation }) => {
         onPressBack={() => navigation.goBack()}
         navigation={navigation}
       />
+      <View style={{ width: '100%', height: 20 }} />
       <View style={[styles.statusBar]}>
         <View
-          style={{ backgroundColor: colors.blue[400], width: '100%' }}
+          style={{ backgroundColor: colors.orange[400], width: '100%' }}
         ></View>
       </View>
       <View style={styles.container}>
