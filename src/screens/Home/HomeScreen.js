d6882fa48a5c37cdf868d0596d99cf0c12f07eba
jaesangkim000 <kjs564821@gmail.com>
2026-01-08 00:49:59 +0900

    style : ios 홈 화면 safeAreaView 추가

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -15,6 +15,7 @@ import RecommendSection from '../../components/home/RecommendSection';
 import CurationCarousel from '../../components/home/CurationCarousel';
 import BannerCard from '../../components/common/BannerCard';
 import { CAROUSEL_DATA } from '../../constants/DummyData';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeSection = ({ title, children, hasDivider = false }) => {
   return (
@@ -36,76 +37,81 @@ const HomeScreen = () => {
   const [hasNewNotification, setHasNewNotification] = useState(true);
 
   return (
-    <ScrollView style={styles.container}>
-      {/* 행사 안내 */}
-      <View style={styles.topArea}>
-        <View style={styles.header}>
-          <View style={styles.headerTopRow}>
-            <Text style={styles.greeting}>사용자님, 안녕하세요</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.container}>
+        {/* 행사 안내 */}
+        <View style={styles.topArea}>
+          <View style={styles.header}>
+            <View style={styles.headerTopRow}>
+              <Text style={styles.greeting}>사용자님, 안녕하세요</Text>
 
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => console.log('알림 클릭')}
-              style={styles.notificationBtn}
-            >
-              <Ionicons
-                name="notifications"
-                size={24}
-                color={colors.gray[300]}
-              />
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => console.log('알림 클릭')}
+                style={styles.notificationBtn}
+              >
+                <Ionicons
+                  name="notifications"
+                  size={24}
+                  color={colors.gray[300]}
+                />
 
-              {hasNewNotification && <View style={styles.badge} />}
+                {hasNewNotification && <View style={styles.badge} />}
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.eventBox}>
+              <Text
+                style={styles.eventText}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                <Text style={styles.boldText}>오늘의 행사</Text> 310관 1층
+                ‘경영경제대학 간식행사’가 18시에 있습니다 🎉
+              </Text>
             </TouchableOpacity>
           </View>
 
-          <TouchableOpacity style={styles.eventBox}>
-            <Text
-              style={styles.eventText}
-              numberOfLines={1}
-              ellipsizeMode="tail"
-            >
-              <Text style={styles.boldText}>오늘의 행사</Text> 310관 1층
-              ‘경영경제대학 간식행사’가 18시에 있습니다 🎉
-            </Text>
-          </TouchableOpacity>
+          {/* 캐러셀 */}
+          <View style={styles.carouselWrapper}>
+            <MainCarousel data={CAROUSEL_DATA} />
+          </View>
         </View>
 
-        {/* 캐러셀 */}
-        <View style={styles.carouselWrapper}>
-          <MainCarousel data={CAROUSEL_DATA} />
-        </View>
-      </View>
+        {/* 이용 가능한 제휴 */}
+        <HomeSection title="🧐 이용 가능한 제휴" hasDivider={false}>
+          <AffiliateSection />
+        </HomeSection>
 
-      {/* 이용 가능한 제휴 */}
-      <HomeSection title="🧐 이용 가능한 제휴" hasDivider={false}>
-        <AffiliateSection />
-      </HomeSection>
+        {/* 공간 추천 */}
+        <HomeSection
+          title="🚀 3시간 공강, 이런 공간은 어때요?"
+          hasDivider={true}
+        >
+          <RecommendSection />
+        </HomeSection>
 
-      {/* 공간 추천 */}
-      <HomeSection title="🚀 3시간 공강, 이런 공간은 어때요?" hasDivider={true}>
-        <RecommendSection />
-      </HomeSection>
+        {/* 큐레이션 */}
+        <HomeSection title="💕 추천 큐레이션" hasDivider={true}>
+          <CurationCarousel />
+        </HomeSection>
 
-      {/* 큐레이션 */}
-      <HomeSection title="💕 추천 큐레이션" hasDivider={true}>
-        <CurationCarousel />
-      </HomeSection>
-
-      <HomeSection title="💯 캠어스를 100% 이용하는 법" hasDivider={true}>
-        <View>
-          <BannerCard
-            title="좋은 제휴 아이디어 공유해주실래요?"
-            subtitle="원하는 제휴 혜택을 학생회에게 직접 제안해요."
-            onPress={() => console.log('제안하기 클릭')}
-          />
-          <BannerCard
-            title="제휴 이용하고 스탬프 받아가세요!"
-            subtitle="제휴만 이용해도 혜택이 팡팡"
-            onPress={() => console.log('스탬프 클릭')}
-          />
-        </View>
-      </HomeSection>
-    </ScrollView>
+        <HomeSection title="💯 캠어스를 100% 이용하는 법" hasDivider={true}>
+          <View>
+            <BannerCard
+              title="좋은 제휴 아이디어 공유해주실래요?"
+              subtitle="원하는 제휴 혜택을 학생회에게 직접 제안해요."
+              onPress={() => console.log('제안하기 클릭')}
+            />
+            <BannerCard
+              title="제휴 이용하고 스탬프 받아가세요!"
+              subtitle="제휴만 이용해도 혜택이 팡팡"
+              onPress={() => console.log('스탬프 클릭')}
+            />
+          </View>
+        </HomeSection>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
