85c2c14fa7fc273ae16868ca0ad575cbf6968185
jaesangkim000 <kjs564821@gmail.com>
2026-01-08 12:43:15 +0900

    fix : ios 문의 작성 스크린에서 외부 클릭시 키보드 내려가지 않는 현상 수정

--- src/screens/MyPage/CreateNewQueryView.js
+++ src/screens/MyPage/CreateNewQueryView.js
@@ -1,4 +1,12 @@
-import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Input from '../../components/Input';
 import colors from '../../style/colors';
 import typography from '../../style/typography';
@@ -23,65 +31,67 @@ const CreateNewQueryView = ({ handleCreateQuery }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Pressable
-        onPress={() => setIsBottomSheetVisible(true)}
-        style={styles.inputWrapper}
-      >
-        <Input
-          placeholder="문의 유형을 선택해주세요"
-          useToggleIcon={true}
-          value={selectedCategory?.name || ''}
-          onlyRead={true}
-          additionalStyle={styles.input}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <Pressable
+          onPress={() => setIsBottomSheetVisible(true)}
+          style={styles.inputWrapper}
+        >
+          <Input
+            placeholder="문의 유형을 선택해주세요"
+            useToggleIcon={true}
+            value={selectedCategory?.name || ''}
+            onlyRead={true}
+            additionalStyle={styles.input}
+          />
+        </Pressable>
+        <View style={styles.textInputWrapper}>
+          <TextInput
+            placeholder="문의 내용을 작성해주세요"
+            style={styles.inqueryInput}
+            value={inqueryContent}
+            onChangeText={setInqueryContent}
+            placeholderTextColor={colors.gray[400]}
+            placeholderStyle={{
+              ...typography.body3Regular,
+              color: colors.gray[400],
+            }}
+            maxLength={1000}
+            multiline={true}
+            numberOfLines={10}
+            textAlignVertical="top"
+            keyboardType="default"
+          />
+          <Text style={styles.inqueryContentLength}>
+            {inqueryContent.length === 0
+              ? '최소 20자 이상'
+              : `${inqueryContent.length}/1000`}
+          </Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button
+            disabled={isButtonDisabled}
+            title="등록하기"
+            style={{
+              width: '100%',
+              height: 50,
+              paddingHorizontal: 10,
+              paddingVertical: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: colors.blue[400],
+              borderRadius: 10,
+            }}
+            onPress={handleCreateQuery}
+          />
+        </View>
+        <CreateQueryBottomSheet
+          isVisible={isBottomSheetVisible}
+          onClose={() => setIsBottomSheetVisible(false)}
+          onSelectCategory={handleSelectCategory}
         />
-      </Pressable>
-      <View style={styles.textInputWrapper}>
-        <TextInput
-          placeholder="문의 내용을 작성해주세요"
-          style={styles.inqueryInput}
-          value={inqueryContent}
-          onChangeText={setInqueryContent}
-          placeholderTextColor={colors.gray[400]}
-          placeholderStyle={{
-            ...typography.body3Regular,
-            color: colors.gray[400],
-          }}
-          maxLength={1000}
-          multiline={true}
-          numberOfLines={10}
-          textAlignVertical="top"
-          keyboardType="default"
-        />
-        <Text style={styles.inqueryContentLength}>
-          {inqueryContent.length === 0
-            ? '최소 20자 이상'
-            : `${inqueryContent.length}/1000`}
-        </Text>
       </View>
-      <View style={styles.buttonWrapper}>
-        <Button
-          disabled={isButtonDisabled}
-          title="등록하기"
-          style={{
-            width: '100%',
-            height: 50,
-            paddingHorizontal: 10,
-            paddingVertical: 15,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: colors.blue[400],
-            borderRadius: 10,
-          }}
-          onPress={handleCreateQuery}
-        />
-      </View>
-      <CreateQueryBottomSheet
-        isVisible={isBottomSheetVisible}
-        onClose={() => setIsBottomSheetVisible(false)}
-        onSelectCategory={handleSelectCategory}
-      />
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -98,6 +108,8 @@ const styles = StyleSheet.create({
   inqueryInput: {
     ...typography.body3Regular,
     color: colors.gray[850],
+    height: 100,
+    width: '100%',
   },
   textInputWrapper: {
     width: '100%',
