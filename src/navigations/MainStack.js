c15abc7378d7a8d14545aa87b0063e295a259e99
jaesangkim000 <kjs564821@gmail.com>
2026-01-08 15:16:40 +0900

    feat : 학생회 로그인시 councilMainTab 이동

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import MainTab from '../components/MainTab';
+import CouncilMainTab from '../components/CouncilMainTab';
 import MapSearchScreen from '../screens/Map/MapSearchScreen';
 import MapSearchResultScreen from '../screens/Map/MapSearchResultScreen';
+
+import useAuthStore from '../store/authStore';
 
 const Stack = createNativeStackNavigator();
 
 const MainStack = () => {
+  const isCouncil = useAuthStore((state) => state?.user?.role === 'COUNCIL');
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="MainTab" component={MainTab} />
+      {isCouncil ? (
+        <Stack.Screen name="MainTab" component={CouncilMainTab} />
+      ) : (
+        <Stack.Screen name="MainTab" component={MainTab} />
+      )}
 
       <Stack.Screen name="MapSearchScreen" component={MapSearchScreen} />
       <Stack.Screen
