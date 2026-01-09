33f3b0d4f13434b9212e4cf5f7d0346d31eb91dd
jaesangkim000 <kjs564821@gmail.com>
2026-01-10 07:15:17 +0900

    style : AffiliateMainScreen 수정

--- src/screens/Affiliation/AffiliationMainScreen.js
+++ src/screens/Affiliation/AffiliationMainScreen.js
@@ -4,13 +4,21 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   ScrollView,
+  FlatList,
+  Pressable,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../../style/colors';
 import typography from '../../style/typography';
+import { useState } from 'react';
 import HostByTab from '../../components/Affiliation/HostByTab';
 import AffiliationCarousel from '../../components/Affiliation/AffiliationCarousel';
 import AffiliationColumnList from '../../components/Affiliation/AffiliationColumnList';
+import {
+  AFFILIATION_COLUMN_LIST_DATA_AFFILIATION,
+  AFFILIATION_COLUMN_LIST_DATA_EVENT,
+} from '../../constants/DummyData';
+import AffiliationColumnListItem from '../../components/Affiliation/AffiliationColumnListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,9 +26,43 @@ const styles = StyleSheet.create({
     backgroundColor: colors.common.white,
     alignItems: 'center',
   },
+  activityTypeSelector: {
+    width: '100%',
+    padding: 20,
+    flexDirection: 'row',
+    gap: 9,
+  },
+  activityTypeSelectorButton: {
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: colors.gray[250],
+    width: 47,
+    height: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activityTypeSelectorButtonText: {
+    ...typography.body4Regular,
+    color: colors.gray[700],
+  },
+  activityTypeSelectorButtonPressed: {
+    borderColor: colors.blue[400],
+  },
+  activityTypeSelectorOrangeButtonPressed: {
+    borderColor: colors.orange[400],
+  },
+  activityTypeSelectorOrangeButtonTextPressed: {
+    ...typography.body4Regular,
+    color: colors.orange[600],
+  },
+  activityTypeSelectorButtonTextPressed: {
+    ...typography.body4Regular,
+    color: colors.blue[600],
+  },
 });
 
 const AffiliationMainScreen = ({ navigation }) => {
+  const [selectedActivityType, setSelectedActivityType] = useState('제휴');
   return (
     <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
       <View style={{ marginTop: 9.5 }}>
@@ -31,7 +73,71 @@ const AffiliationMainScreen = ({ navigation }) => {
         />
       </View>
       <AffiliationCarousel />
-      <AffiliationColumnList navigation={navigation} />
+      {/* <AffiliationColumnList navigation={navigation} /> */}
+      <View style={styles.activityTypeSelector}>
+        <Pressable
+          style={[
+            styles.activityTypeSelectorButton,
+            selectedActivityType === '제휴'
+              ? styles.activityTypeSelectorButtonPressed
+              : styles.activityTypeSelectorButton,
+          ]}
+          onPress={() => setSelectedActivityType('제휴')}
+        >
+          <Text
+            style={
+              selectedActivityType === '제휴'
+                ? styles.activityTypeSelectorButtonTextPressed
+                : styles.activityTypeSelectorButtonText
+            }
+          >
+            제휴
+          </Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.activityTypeSelectorButton,
+            selectedActivityType === '행사'
+              ? styles.activityTypeSelectorButtonPressed
+              : styles.activityTypeSelectorButton,
+          ]}
+          onPress={() => setSelectedActivityType('행사')}
+        >
+          <Text
+            style={
+              selectedActivityType === '행사'
+                ? styles.activityTypeSelectorButtonTextPressed
+                : styles.activityTypeSelectorButtonText
+            }
+          >
+            행사
+          </Text>
+        </Pressable>
+      </View>
+      {selectedActivityType === '제휴' && (
+        <FlatList
+          style={{ width: '100%' }}
+          showsVerticalScrollIndicator={true}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+          data={AFFILIATION_COLUMN_LIST_DATA_AFFILIATION}
+          renderItem={({ item }) => (
+            <AffiliationColumnListItem item={item} navigation={navigation} />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      )}
+      {selectedActivityType === '행사' && (
+        <FlatList
+          style={{ width: '100%' }}
+          showsVerticalScrollIndicator={true}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+          data={AFFILIATION_COLUMN_LIST_DATA_EVENT}
+          renderItem={({ item }) => (
+            <AffiliationColumnListItem item={item} navigation={navigation} />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      )}
     </SafeAreaView>
   );
 };
