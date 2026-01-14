32631224ef0696e26c193d372f8f6131b320debe
jaesangkim000 <kjs564821@gmail.com>
2026-01-14 18:12:30 +0900

    fix : 행사 카루셀 아이템 정보 추가

--- src/components/Affiliation/AffiliationCarouselItem.js
+++ src/components/Affiliation/AffiliationCarouselItem.js
@@ -150,7 +150,7 @@ const AffiliationCarouselItem = ({
         <View style={styles.placeContainer}>
           <Vector2 width={9.6} height={12.4} color={colors.gray[300]} />
           <Text style={styles.place} numberOfLines={1} ellipsizeMode="tail">
-            {item.place} {item.detailedLocation}
+            {item.place || item.placeName} {item.detailedLocation}
           </Text>
         </View>
         <View style={styles.dateContainer}>
