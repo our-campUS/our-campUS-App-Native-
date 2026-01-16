4b6903d7395bddd8f1a23b2bd394b268df5a0de6
daeun088 <kde2329@naver.com>
2026-01-16 21:10:06 +0900

    style: 홈 화면 StoreCard icon 수정

--- src/components/common/StoreCard.js
+++ src/components/common/StoreCard.js
@@ -3,6 +3,9 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import theme from '../../style';
+import StarIcon from '../../../assets/icons/common/star.svg';
+import TicketIcon from '../../../assets/icons/common/ticket.svg';
+import PinIcon from '../../../assets/icons/common/pin.svg';
 
 const StoreCard = ({
   image,
@@ -36,27 +39,17 @@ const StoreCard = ({
         </View>
 
         <View style={styles.infoRow}>
-          <Ionicons name="star" size={14} color="black" style={styles.icon} />
+          <StarIcon width={20} height={20} style={{ marginRight: 4 }} />
           <Text style={styles.infoText}>{rating}</Text>
         </View>
 
         <View style={styles.infoRow}>
-          <MaterialCommunityIcons
-            name="ticket-percent"
-            size={14}
-            color="black"
-            style={styles.icon}
-          />
+          <TicketIcon width={20} height={20} style={{ marginRight: 4 }} />
           <Text style={styles.infoText}>{discount}</Text>
         </View>
 
         <View style={styles.infoRow}>
-          <Ionicons
-            name="location-sharp"
-            size={14}
-            color="black"
-            style={styles.icon}
-          />
+          <PinIcon width={20} height={20} style={{ marginRight: 4 }} />
           <Text style={styles.infoText}>{distance}</Text>
         </View>
       </View>
