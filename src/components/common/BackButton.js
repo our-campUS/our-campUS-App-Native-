346fcaee29e9f78ff81169e49af5f3f3a3b720f5
MinSeokKim <kimjunseok23@gmail.com>
2026-03-17 23:46:12 +0900

    feat: 뒤로가기 버튼 컴포넌트로 분리

---
+++ src/components/common/BackButton.js
@@ -0,0 +1,24 @@
+import { Pressable, StyleSheet } from 'react-native';
+import BackIcon from '@assets/back.svg';
+import colors from '@style/colors';
+
+const BackButton = ({ onPress, color = colors.gray[800], style }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+      style={[styles.container, style]}
+    >
+      <BackIcon width={10} height={16} color={color} />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    zIndex: 1,
+  },
+});
+
+export default BackButton;
