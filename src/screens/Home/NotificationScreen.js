d9543fb0a21d35d9050ed99c0ae50ca8a56415e3
MinSeokKim <kimjunseok23@gmail.com>
2026-03-25 03:52:00 +0900

    refactor(notification): 요청에 따라 변경된 API에서 반환하는 councilType을 사용하도록 수정

--- src/screens/Home/NotificationScreen.js
+++ src/screens/Home/NotificationScreen.js
@@ -57,9 +57,9 @@ const NotificationScreen = () => {
     // 타입별 화면 이동
     switch (notification.type) {
       case 'COUNCIL_POST_CREATED':
-        // TODO: 알림 API에 councilType 필드 추가 시 councilType도 전달 (추천 목록 조회용)
         navigation.navigate('AffiliationDetailScreen', {
           item: { id: notification.referenceId },
+          councilType: notification.councilType,
         });
         break;
       case 'REWARD_GRANTED':
