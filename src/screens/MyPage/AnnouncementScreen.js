705c5f3bd84f46542191f58e1485aa883835d298
MinSeokKim <kimjunseok23@gmail.com>
2026-04-03 02:11:36 +0900

    refactor(announcement): 공지사항 페이지에서 임시로 emptyresult를 보여주도록 수정

--- src/screens/MyPage/AnnouncementScreen.js
+++ src/screens/MyPage/AnnouncementScreen.js
@@ -1,11 +1,9 @@
-import { View, Text, StyleSheet, FlatList, Pressable } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import LabelTitle from '../../components/LabelTitle';
 import colors from '../../style/colors';
-import typography from '../../style/typography';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AnnouncementTypeSelector from '../../components/MyPage/AnnouncementTypeSelector';
-import { ANNOUNCEMENT_DATA } from '../../constants/DummyData';
-import NewDot from '../../../assets/RedDot.svg';
+import EmptyResult from '../../components/common/EmptyResult';
 
 const AnnouncementScreen = ({ navigation }) => {
   return (
@@ -15,25 +13,14 @@ const AnnouncementScreen = ({ navigation }) => {
         useBackButton={true}
         onPressBack={() => navigation.goBack()}
       />
-      {/* <View style={{ width: '100%', height: 20 }} /> */}
       <AnnouncementTypeSelector />
       <View style={{ width: '100%', height: 10 }} />
       <FlatList
-        data={ANNOUNCEMENT_DATA}
-        renderItem={({ item }) => (
-          <Pressable
-            style={styles.announcementItem}
-            onPress={() =>
-              navigation.navigate('AnnouncementDetailScreen', { item })
-            }
-          >
-            <View style={styles.titleWrapper}>
-              <Text style={styles.announcementTitle}>{item.title}</Text>
-              {item.isNew && <NewDot />}
-            </View>
-            <Text style={styles.announcementDate}>{item.date}</Text>
-          </Pressable>
-        )}
+        data={[]}
+        renderItem={() => null}
+        ListEmptyComponent={
+          <EmptyResult message={'등록된 공지사항이 없습니다.'} />
+        }
       />
     </SafeAreaView>
   );
@@ -42,27 +29,7 @@ const AnnouncementScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  },
-  announcementItem: {
-    paddingVertical: 16,
-    paddingHorizontal: 20,
-    gap: 6,
-    borderBottomWidth: 0.5,
-    borderBottomColor: colors.gray[200],
-  },
-  titleWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'relative',
-  },
-  announcementTitle: {
-    ...typography.body3Regular,
-    color: colors.gray[850],
-  },
-  announcementDate: {
-    ...typography.caption1Regular,
-    color: colors.gray[600],
+    backgroundColor: colors.common.white,
   },
 });
 
