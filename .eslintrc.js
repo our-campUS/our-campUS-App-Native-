f11ccf30ca4f9c6f8658a9603c44d9739108100e
MinSeokKim <kimjunseok23@gmail.com>
2026-04-02 16:52:07 +0900

    feat(lint): ESLint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-console': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/no-color-literals': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+  },
 };
